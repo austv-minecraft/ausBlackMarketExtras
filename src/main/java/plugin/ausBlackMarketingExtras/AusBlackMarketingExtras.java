6431f6c5672aad129571ac3bd013bee14d50cf5f
ZzPowerTech <surikengamer@gmail.com>
2026-04-26 16:50:56 -0300

    first commit

---
+++ src/main/java/plugin/ausBlackMarketingExtras/AusBlackMarketingExtras.java
@@ -0,0 +1,16 @@
+package plugin.ausBlackMarketingExtras;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AusBlackMarketingExtras extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/plugin/ausBlackMarketingExtras/AusBlackMarketingExtrasBootstrap.java
@@ -0,0 +1,12 @@
+package plugin.ausBlackMarketingExtras;
+
+import io.papermc.paper.plugin.bootstrap.BootstrapContext;
+import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
+
+class AusBlackMarketingExtrasBootstrap implements PluginBootstrap {
+
+    @Override
+    public void bootstrap(final BootstrapContext context) {
+        // Plugin bootstrap logic
+    }
+}
